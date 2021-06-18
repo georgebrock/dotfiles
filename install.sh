2e060f3df940abc41de4cddac5e2f64d97f2377a
George Brocklehurst <george@georgebrock.com>
2021-06-18 19:08:33 +0100

    Add setup script.
    This is primarily for GitHub Codespaces, so that my dotfiles will be used
    automatically.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+
+if [[ "$CODESPACES" = "true" ]]; then
+  sudo apt-get install rcm tmux
+  rcup -v -t linux -t development
+elif [[ "$(uname)" = "Darwin" ]]; then
+  brew install rcm
+  rcup -v -t macos
+  rcup -v
+else
+  >&2 echo "error: Unknown system"
+  exit 1
+fi
+
+if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qa
