e9c5da25f9ab17bbe72832fec02585eb27118f6f
George Brocklehurst <george@georgebrock.com>
2022-06-20 12:10:26 +0100

    Better ctags.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -x
 
 if [[ "$CODESPACES" = "true" ]]; then
   rm ~/.bashrc
-  sudo apt-get install -y rcm tmux exuberant-ctags
+  sudo apt-get install -y rcm tmux universal-ctags
   rcup -f -v -d . -t linux -t development
 
   if [[ -d /etc/ssh ]]; then
